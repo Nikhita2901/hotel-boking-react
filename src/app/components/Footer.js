40fa7d7541c55711f91322d8746e454381b7c147
Nikhita2901 <nikhita.s2901@gmail.com>
2020-10-12 01:20:23 +0530

    Added footer component

---
+++ src/app/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p style={{float:"right",color:"lightgray"}}>NHMS@2020</p>
+  </div>
+);
+
+export default Footer;
